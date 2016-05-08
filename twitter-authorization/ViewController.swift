1d0726c13bac6824f997c1bba78f62646922fb48
star__hoshi <kensuke1751@gmail.com>
2016-05-08 10:41:13 +0900

    set event.

--- twitter-authorization/ViewController.swift
+++ twitter-authorization/ViewController.swift
@@ -9,17 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
+  @IBOutlet weak var textView: UITextView!
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
   }
-
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
-
+  
+  
+  @IBAction func clickLogin(sender: AnyObject) {
+  }
+  
+  
+  @IBAction func clickLogout(sender: AnyObject) {
+  }
+  
+  
+  @IBAction func clickFetch(sender: AnyObject) {
+  }
+  
 }
-
